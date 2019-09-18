e7ba850dd9c429a6ba256e6d29138eacea250ff8
Dominique Skaf <doskaf@yahoo.com>
2019-09-18 19:30:48 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,9 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(".")
-    self.split("?")
-    self.split("!")
-    self.count
+    self.split(".").count
+    self.split("?").count
+    self.split("!").count
   end
 end
